0e8235322e8b9d34a7816340fce90da8e1775f50
justindstein <jaydstein@gmail.com>
2023-07-22 13:13:59 -0300

    Add a vehicle with driving physics and an obstacle with collision physics.

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float SteerSpeed;
+    [SerializeField] private float MoveSpeed;
+
+    private const string HORIZONTAL_AXIS = "Horizontal";
+    private const string VERTICAL_AXIS = "Vertical";
+
+    private float movementInput;
+    private float steerInput;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        this.movementInput = Input.GetAxis(VERTICAL_AXIS);
+        this.steerInput = Input.GetAxis(HORIZONTAL_AXIS);
+
+        if (movementInput != 0)
+        {
+            //transform.Translate(new Vector3(0, (movementInput * MoveSpeed) * Time.deltaTime, 0), Space.Self);
+            transform.Translate(0, movementInput * MoveSpeed * Time.deltaTime, 0);
+        }
+
+        if (steerInput != 0)
+        {
+            transform.Rotate(0, 0, steerInput * SteerSpeed * Time.deltaTime);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
