13d853a96e5e5b8fcecccbd3c30a72206396d5e5
justindstein <jaydstein@gmail.com>
2023-08-07 15:04:43 -0300

    BeaconManager cleanup.

--- Assets/Scripts/Managers/BeaconManager.cs
+++ Assets/Scripts/Managers/BeaconManager.cs
@@ -3,27 +3,25 @@ using UnityEngine;
 
 public class BeaconManager : MonoBehaviour
 {
-    // TODO: can this be changed to prefab?
-    public GameObject PickupBeaconTrigger;
-    // TODO: can this be changed to prefab?
-    public GameObject DeliveryBeaconTrigger;
+    public GameObject PickupBeaconTriggerPrefab;
+    public GameObject DeliveryBeaconTriggerPrefab;
 
-    private List<GameObject> beaconSpawns;
+    private List<GameObject> beaconSpawners;
 
     public void Awake()
     {
-        this.beaconSpawns = new List<GameObject>(GameObject.FindGameObjectsWithTag("BeaconSpawnPoint"));
-        GameObject randomBeaconSpawn = CollectionUtil.GetRandomElement(this.beaconSpawns);
+        this.beaconSpawners = new List<GameObject>(GameObject.FindGameObjectsWithTag("BeaconSpawnPoint"));
+        GameObject randomBeaconSpawner = CollectionUtil.GetRandomElement(this.beaconSpawners);
 
-        this.spawnBeacon(this.PickupBeaconTrigger, randomBeaconSpawn);
+        this.spawnBeacon(this.PickupBeaconTriggerPrefab, randomBeaconSpawner);
     }
 
     /*
      * Instantiates a new beacon of type gameObject at the provided position and rotation.
      */
-    private GameObject spawnBeacon(GameObject gameObject, GameObject activeBeaconSpawn) // TODO: activeBeaconSpawn -> beaconSpawn and gameObject -> prefabTemplate
+    private GameObject spawnBeacon(GameObject gameObjectTemplate, GameObject activeBeaconSpawn)
     {
-        GameObject instance = Instantiate(gameObject, activeBeaconSpawn.transform.position, activeBeaconSpawn.transform.rotation);
+        GameObject instance = Instantiate(gameObjectTemplate, activeBeaconSpawn.transform.position, activeBeaconSpawn.transform.rotation);
         instance.GetComponent<BeaconTrigger>().setParentGameObject(activeBeaconSpawn);
         return instance;
     }
@@ -39,8 +37,8 @@ public class BeaconManager : MonoBehaviour
         Debug.Log(string.Format("BeaconManager.PackagePickup: [sender: {0}] [dataL {1}]", sender, data));
 
         Destroy(sender.gameObject);
-        GameObject randomBeaconSpawn = CollectionUtil.GetRandomElementExcluding(this.beaconSpawns, new List<GameObject>() { sender.GetComponent<BeaconTrigger>().getParentGameObject() });
-        this.spawnBeacon(this.DeliveryBeaconTrigger, randomBeaconSpawn);
+        GameObject randomBeaconSpawner = CollectionUtil.GetRandomElementExcluding(this.beaconSpawners, new List<GameObject>() { sender.GetComponent<BeaconTrigger>().getParentGameObject() });
+        this.spawnBeacon(this.DeliveryBeaconTriggerPrefab, randomBeaconSpawner);
     }
 
     /// <summary>
@@ -54,7 +52,7 @@ public class BeaconManager : MonoBehaviour
         Debug.Log(string.Format("BeaconManager.PackageDelivered: [sender: {0}] [dataL {1}]", sender, data));
 
         Destroy(sender.gameObject);
-        GameObject randomBeaconSpawn = CollectionUtil.GetRandomElementExcluding(this.beaconSpawns, new List<GameObject>() { sender.GetComponent<BeaconTrigger>().getParentGameObject() });
-        this.spawnBeacon(this.PickupBeaconTrigger, randomBeaconSpawn);
+        GameObject randomBeaconSpawner = CollectionUtil.GetRandomElementExcluding(this.beaconSpawners, new List<GameObject>() { sender.GetComponent<BeaconTrigger>().getParentGameObject() });
+        this.spawnBeacon(this.PickupBeaconTriggerPrefab, randomBeaconSpawner);
     }
 }
